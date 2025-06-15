08af490557196be99ba3e1684ff8086431e9fb56
Rayapureddyhemasundhar <rayapureddysundhar@gmail.com>
2025-06-15 22:17:24 +0530

    Create printing_alpha_small.cpp

---
+++ basics/printing_alpha_small.cpp
@@ -0,0 +1,13 @@
+// printing alpha small
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 97; i <= 122; i++)
+    {
+
+        cout << char(i) << endl;
+    }
+    return 0;
+}

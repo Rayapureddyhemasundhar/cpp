11cb3139528d77e44a190e71da583a5a4a4b431d
Rayapureddyhemasundhar <rayapureddysundhar@gmail.com>
2025-06-15 22:15:07 +0530

    Create printing_alpha_cap.cpp

---
+++ basics/printing_alpha_cap.cpp
@@ -0,0 +1,13 @@
+// printing alpha cap
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 65; i <= 90; i++)
+    {
+
+        cout << char(i) << endl;
+    }
+    return 0;
+}

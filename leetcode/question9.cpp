96586bdc7019c887bba403993840513521a69469
Rayapureddyhemasundhar <rayapureddysundhar@gmail.com>
2025-09-29 22:49:59 +0530

    Create question9.cpp

---
+++ leetcode/question9.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+
+        if (x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+        int reversed = 0;
+        int original = x;
+
+        while (x > 0)
+        {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+
+            if (reversed >= x)
+                break;
+        }
+
+        return (reversed == x || reversed / 10 == x);
+    }
+};
+// https://leetcode.com/problems/palindrome-number/description/

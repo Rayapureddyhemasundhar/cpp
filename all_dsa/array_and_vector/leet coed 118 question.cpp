9120e44464c04a8a49974a44fe9190fb6f9ba721
Rayapureddyhemasundhar <rayapureddysundhar@gmail.com>
2025-12-18 10:56:47 +0530

    Create leet coed 118 question.cpp

---
+++ all_dsa/array_and_vector/leet coed 118 question.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>> v;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1);
+            v.push_back(row);
+        }
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    v[i][j] = 1;
+                }
+                else {
+                    v[i][j] = v[i - 1][j] + v[i - 1][j - 1];
+                }
+            }
+        }
+
+        return v;
+    }
+};

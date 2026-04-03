bff91550c4e52e56e876a099233a6e54ee15ccd8
Rayapureddyhemasundhar <198905187+Rayapureddyhemasundhar@users.noreply.github.com>
2026-04-03 23:31:27 +0530

    Create ran4.cpp

---
+++ leetcode/ran4.cpp
@@ -0,0 +1,20 @@
+struct ListNode {
+    int val;
+    ListNode* next;
+};
+
+bool hasCycle(ListNode *head) {
+    if (!head) return false;
+
+    ListNode *slow = head, *fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}

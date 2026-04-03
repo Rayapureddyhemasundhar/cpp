6a9e696965030239eb38a24d8bb596bc96df2090
Rayapureddyhemasundhar <198905187+Rayapureddyhemasundhar@users.noreply.github.com>
2026-04-03 23:31:23 +0530

    Create ran3.cpp

---
+++ leetcode/ran3.cpp
@@ -0,0 +1,19 @@
+#include <unordered_set>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    unordered_set<char> st;
+    int left = 0, maxLen = 0;
+
+    for (int right = 0; right < s.size(); right++) {
+        while (st.find(s[right]) != st.end()) {
+            st.erase(s[left]);
+            left++;
+        }
+
+        st.insert(s[right]);
+        maxLen = max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}

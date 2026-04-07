efb67ec253d790bd7aa934b24ba6266e077b404b
Rayapureddyhemasundhar <198905187+Rayapureddyhemasundhar@users.noreply.github.com>
2026-04-08 00:00:25 +0530

    Update new34.cpp

---
+++ leetcode/new34.cpp
@@ -0,0 +1,54 @@
+#include <sstream>
+
+string serialize(TreeNode* root) {
+    if (!root) return "";
+
+    queue<TreeNode*> q;
+    q.push(root);
+    string res;
+
+    while (!q.empty()) {
+        TreeNode* node = q.front(); q.pop();
+
+        if (!node) {
+            res += "N,";
+            continue;
+        }
+
+        res += to_string(node->val) + ",";
+        q.push(node->left);
+        q.push(node->right);
+    }
+
+    return res;
+}
+
+TreeNode* deserialize(string data) {
+    if (data.empty()) return NULL;
+
+    stringstream ss(data);
+    string item;
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode{stoi(item)};
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* node = q.front(); q.pop();
+
+        getline(ss, item, ',');
+        if (item != "N") {
+            node->left = new TreeNode{stoi(item)};
+            q.push(node->left);
+        }
+
+        getline(ss, item, ',');
+        if (item != "N") {
+            node->right = new TreeNode{stoi(item)};
+            q.push(node->right);
+        }
+    }
+
+    return root;
+}
